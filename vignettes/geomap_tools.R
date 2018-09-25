2f682d54f94d8c6e91fc57adf7b6407ee9ad3315
Mark Zwart <markz@corp.graydon.nl>
2018-09-25 13:53:38 +0200

    Bug fixes

--- vignettes/geomap_tools.R
+++ vignettes/geomap_tools.R
@@ -1,5 +1,7 @@
 ## ----setup, include = FALSE----------------------------------------------
 library(tmap)
+library(dplyr)
+library(magrittr)
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -13,9 +15,11 @@ sp_companies_uk <- create_spatial_df(tbl_companies_uk, lon = "LONGITUDE_RA", lat
 
 ## ---- message=FALSE------------------------------------------------------
 library(tmap)
+
 tmap_mode("plot")
 
-  tm_shape(sp_uk_lieutenancy, name = "Lieutenancy borders") +
+# Creating the Lieutenancy borders 
+tm_shape(sp_uk_lieutenancy, name = "Lieutenancy borders") +
   tm_layout(frame = FALSE) +
   tm_fill(col = col_graydon[4],
           title = "Lieutenancy",
@@ -23,7 +27,8 @@ tmap_mode("plot")
           legend.show = TRUE)  +
   tm_borders(col = "white",
              alpha = 0.2) +
-    tm_shape(sp_companies_uk, name = "Companies") +
+# Adding the company dots  
+tm_shape(sp_companies_uk, name = "Companies") +
   tm_layout(aes.color = c(fill = col_graydon[1],
                           borders = col_graydon[1], 
                           symbols = col_graydon[1]),
@@ -33,7 +38,26 @@ tmap_mode("plot")
           alpha = 0.7,
           legend.show = FALSE) 
 
-
 ## ---- message=FALSE, warning=FALSE---------------------------------------
 tbl_merge <- match_sp(sp_companies_uk, sp_uk_lieutenancy)
+
+## ------------------------------------------------------------------------
+tbl_companies_region <- tbl_merge %>% 
+  mutate(id_merge = as.character(id_merge)) %>% 
+  group_by(id_merge) %>% 
+  summarise(qty_companies = n())
+
+## ------------------------------------------------------------------------
+sp_uk_lieutenancy@data %<>%
+  left_join(tbl_companies_region, by = "id_merge")
+
+## ------------------------------------------------------------------------
+tm_shape(sp_uk_lieutenancy, name = "Lieutenancy borders") +
+  tm_layout(aes.palette = list(seq = c(col_graydon_low, col_graydon_high)), frame = FALSE) +
+  tm_fill(col = "qty_companies",
+          title = "Lieutenancy",
+          alpha = .8,
+          legend.show = TRUE)  +
+  tm_borders(col = "white",
+             alpha = 0.2)
 
