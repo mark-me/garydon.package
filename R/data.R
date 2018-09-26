d28d5e2c6e6cb702215e393c2a7ea4971195736f
Mark Zwart <markz@corp.graydon.nl>
2018-09-26 14:41:44 +0200

    Adding data documentation

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Set of Graydon company colors
+#'
+#' A vector containing 9 Graydon company colors in hexadecimal format.
+#'
+"col_graydon"
+
+#' Low value color
+#'
+#' A variable containing a Graydon company color used for the lowest value in positive
+#' valued gradients. The color value is in hexadecimal format.
+#'
+"col_graydon_low"
+
+#' High value color
+#'
+#' A variable containing a Graydon company color used for the highest value in positive
+#' valued gradients. The color value is in hexadecimal format.
+#'
+"col_graydon_high"
