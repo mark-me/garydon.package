df023580ab5b647b941d1350b7ab689a5511a265
Mark Zwart <markz@corp.graydon.nl>
2018-09-27 09:22:06 +0200

    Added dplyr scoping

--- R/economic_activity_hierachy.R
+++ R/economic_activity_hierachy.R
@@ -1,4 +1,4 @@
-#' A function for applying column configuration file
+#' A function to determine which economic activity definitiion level is optimal if you want to have a minimum value in a economic activity group
 #'
 #' This function can be used to aggregate a NACE or SBI code economic activity tree,
 #' so the codes represent enough of something for example number of companies,
@@ -20,29 +20,19 @@
 #'   \item{code_new}{The replacement code, which indicates the new position in the hierarchy}
 #'   \item{value}{The value associated with the original code (input)}
 #' }
-#' @keywords config import data
+#' @keywords SBI NACE SIC
 #' @export
 #' @examples
-#' get_columns_file(df, filename_source)
-
-
-# Arguments:
-#   * tbl_hierarchy - a data frame that should contain the entire NACE or SBI hierarchy with a
-#       quantity or amount. It should contain the following columns:
-#         - code        - a NACE or SBI code.
-#         - code_parent - a NACE or SBI code that refers to the direct parent code.
-#         - layer_no    - an integer indicating the hierarchie's level, 1 being the top level
-#         - value       -
-
+#' roll_up_hierarchy(tbl_hierarchy, threshold = 200)
 roll_up_hierarchy <- function(tbl_hierarchy, threshold) {
 
   # End-points of the roll-up
-  tbl_hierarchy_aggr <- data_frame(code = as.character(),
+  tbl_hierarchy_aggr <- data.frame(code = as.character(),
                                    code_new = as.character(),
                                    value = as.integer())
 
   # Codes that are pushed up the tree (code has too low value to be end-point)
-  tbl_level_migrating <- data_frame(code = as.character(),
+  tbl_level_migrating <- data.frame(code = as.character(),
                                     code_parent = as.character(),
                                     value = as.integer())
 
@@ -53,77 +43,82 @@ roll_up_hierarchy <- function(tbl_hierarchy, threshold) {
 
     # Get the current level
     tbl_level <- tbl_hierarchy %>%
-      filter(layer_no == i)  %>%
-      mutate(code_orig = code) %>%
-      select(code, code_parent, code_orig, value)
+      dplyr::filter(layer_no == i)  %>%
+      dplyr::mutate(code_orig = code) %>%
+      dplyr::select(code, code_parent, code_orig, value)
 
     # Make previous level codes have parent code of their parents
     tbl_level_prev <- tbl_level %>%
-      select(-value, -code_orig) %>%
-      inner_join(tbl_level_migrating, by = c("code" = "code_parent")) %>%
-      rename(code_orig = code.y)
+      dplyr::select(-value, -code_orig) %>%
+      dplyr::inner_join(tbl_level_migrating, by = c("code" = "code_parent")) %>%
+      dplyr::rename(code_orig = code.y)
 
     # Calculate this level's and previous level values per code
     tbl_up_or_down <- rbind(tbl_level, tbl_level_prev) %>%
-      group_by(code, code_parent) %>%
-      mutate(value_code = sum(value, na.rm = TRUE)) %>%
-      ungroup()
+      dplyr::group_by(code, code_parent) %>%
+      dplyr::mutate(value_code = sum(value, na.rm = TRUE)) %>%
+      dplyr::ungroup()
 
     # Identify the codes that have a sufficient quantity to stay at the current level
     tbl_stays <- tbl_up_or_down %>%
-      filter(value_code >= threshold & value > 0) %>%
-      mutate(code_new = code,
-             code = code_orig) %>%
-      select(code, code_new, value)
+      dplyr::filter(value_code >= threshold & value > 0) %>%
+      dplyr::mutate(code_new = code,
+                    code = code_orig) %>%
+      dplyr::select(code, code_new, value)
 
     # Add the codes that stay behind to the result table
     tbl_hierarchy_aggr <- rbind(tbl_hierarchy_aggr, tbl_stays)
 
     # Gather the codes that need to be moved up further
     tbl_level_migrating <- tbl_up_or_down %>%
-      filter(value > 0 &
-               value_code < threshold & value_code > 0) %>%
-      mutate(code = code_orig) %>%
-      select(code, code_parent, value)
+      dplyr::filter(value > 0 & value_code < threshold & value_code > 0) %>%
+      dplyr::mutate(code = code_orig) %>%
+      dplyr::select(code, code_parent, value)
 
   }
 
   # Handling the top level
   # Get the current level
   tbl_level <- tbl_hierarchy %>%
-    filter(layer_no == i - 1 & value > 0)  %>%
-    mutate(code_new = code) %>%
-    select(code, code_new, value)
+    dplyr::filter(layer_no == i - 1 & value > 0)  %>%
+    dplyr::mutate(code_new = code) %>%
+    dplyr::select(code, code_new, value)
 
   # Place all data that hasn't been placed yet on the top level
   tbl_remainder <- rbind(tbl_level,
                          tbl_level_migrating %>%
-                           rename(code_new = code_parent))
+                           dplyr::rename(code_new = code_parent))
 
   tbl_hierarchy_aggr <- rbind(tbl_hierarchy_aggr, tbl_remainder)
 
 }
 
-# Function: hierarchy_code_level ------------------------------------------------------------
-
-# Purpose: This function can be used enrich each of the codes from it's chosen level's
-#           code value
-
-# Arguments:
-#   * tbl_hierarchy - a data frame that should contain the entire NACE or SBI hierarchy,
-#         having at least the following columns:
-#         - code        - a NACE or SBI code.
-#         - code_parent - a NACE or SBI code that refers to the direct parent code.
-#         - layer_no    - an integer indicating the hierarchie's level, 1 being the top level
-#   * level_no - an integer indicating the level, 1 being the top most level (e.g. A, B etc.)
-
-# Return value: a data frame containing the complete tbl_hierarchy data frame, with the new column:
-#   * code_level_x  - The code at the chosen level of the hierarchy
-#
+#' A function for enriching each of the codes from it's chosen level's code value
+#'
+#' @param tbl_hierarchy  a data frame that should contain the entire NACE or SBI hierarchy with a
+#' quantity or amount. It should contain the following columns: \describe{
+#'   \item{code}{a NACE or SBI code.}
+#'   \item{code_parent}{a NACE or SBI code that refers to the direct parent code.}
+#'   \item{layer_no}{an integer indicating the hierarchy's level, 1 being the top level}
+#'   \item{value}{the quantity that is used to evaluate whether the code should be kept in
+#'   place or should be 'pushed up' the hierarchy.}
+#' }
+#' @param level_no
+#' @param tbl_hierarchy  a data frame that should contain the entire NACE or SBI hierarchy with
+#' the added code from the requested level: \describe{
+#'   \item{code}{a NACE or SBI code.}
+#'   \item{code_parent}{a NACE or SBI code that refers to the direct parent code.}
+#'   \item{layer_no}{an integer indicating the hierarchy's level, 1 being the top level}
+#'   \item{code_level_x}{The code at the chosen level of the hierarchy}
+#' }
+#' @keywords SBI NACE SIC
+#' @export
+#' @examples
+#' hierarchy_code_level(tbl_hierarchy, level_no = 2)
 hierarchy_code_level <- function(tbl_hierarchy, level_no){
 
   # Codes that are pushed up the tree
-  tbl_level_stored <- data_frame(code = as.character(),
+  tbl_level_stored <- data.frame(code = as.character(),
                                  code_parent = as.character())
 
   # Iterator for the levels in the hierarchy, from top to specified level
@@ -133,14 +128,14 @@ hierarchy_code_level <- function(tbl_hierarchy, level_no){
 
     # Get the current level
     tbl_level <- tbl_hierarchy %>%
-      filter(layer_no == i)  %>%
-      select(code, code_parent)
+      dplyr::filter(layer_no == i)  %>%
+      dplyr::select(code, code_parent)
 
     # Push parent_code to the next level
     tbl_level_next <- tbl_level %>%
-      left_join(tbl_level_stored, by = c("code_parent" = "code")) %>%
-      mutate(code_parent = ifelse(!is.na(code_parent.y), code_parent.y, code_parent)) %>%
-      select(code, code_parent)
+      dplyr::left_join(tbl_level_stored, by = c("code_parent" = "code")) %>%
+      dplyr::mutate(code_parent = ifelse(!is.na(code_parent.y), code_parent.y, code_parent)) %>%
+      dplyr::select(code, code_parent)
 
     # Store result
     tbl_level_stored <- rbind(tbl_level_stored, tbl_level_next)
@@ -155,7 +150,9 @@ hierarchy_code_level <- function(tbl_hierarchy, level_no){
 
   # Add code back to original data frame
   tbl_hierarchy %<>%
-    left_join(tbl_level_stored, by = "code")
+    dplyr::left_join(tbl_level_stored, by = "code")
+
+  return(tbl_hierarchy)
 }
 
 # Function: hierarchy_code_all ------------------------------------------------------------
@@ -205,7 +202,7 @@ clean_hierarchy <- function(tbl_hierarchy) {
   levels <- sort(unique(tbl_hierarchy$layer_no), decreasing = TRUE)
 
   # Codes that have quantities of connective codes
-  tbl_level_codes <- data_frame(code = as.character(),
+  tbl_level_codes <- data.frame(code = as.character(),
                                 code_parent = as.character(),
                                 layer_no = as.integer(),
                                 value = as.numeric(),
@@ -215,22 +212,22 @@ clean_hierarchy <- function(tbl_hierarchy) {
 
     # Get the current level
     tbl_level <- tbl_hierarchy %>%
-      filter(layer_no == i)  %>%
-      select(code, code_parent, layer_no, value)
+      dplyr::filter(layer_no == i)  %>%
+      dplyr::select(code, code_parent, layer_no, value)
 
     # Joing previous layer to see whether codes are connective
     tbl_level_prev <- tbl_level %>%
-      left_join(tbl_level_codes, by = c("code" = "code_parent")) %>%
-      mutate(layer_no = i,
-             has_child = !is.na(code.y)) %>%
-      rename(value = value.x) %>%
-      group_by(code, code_parent, layer_no, value) %>%
-      summarise(has_child = max(has_child)) %>%
-      ungroup()
+      dplyr::left_join(tbl_level_codes, by = c("code" = "code_parent")) %>%
+      dplyr::mutate(layer_no = i,
+                    has_child = !is.na(code.y)) %>%
+      dplyr::rename(value = value.x) %>%
+      dplyr::group_by(code, code_parent, layer_no, value) %>%
+      dplyr::summarise(has_child = max(has_child)) %>%
+      dplyr::ungroup()
 
     # Remove codes that have no value and are not connective
     tbl_level_prev %<>%
-      filter(has_child == 1 | (!is.na(value) & value != 0))
+      dplyr::filter(has_child == 1 | (!is.na(value) & value != 0))
 
     tbl_level_codes <- rbind(tbl_level_codes, tbl_level_prev)
   }
@@ -253,12 +250,11 @@ clean_hierarchy <- function(tbl_hierarchy) {
 
 plot_hierarchy <- function(tbl_hierarchy, title = ""){
 
-  library(ggraph)
   library(igraph)
 
-  tbl_nodes <- data_frame(code = with(tbl_hierarchy, unique(c(code, code_parent)))) %>%
-    left_join(tbl_hierarchy, by = "code") %>%
-    mutate(value = ifelse(is.na(value), 0, value),
+  tbl_nodes <- data.frame(code = with(tbl_hierarchy, unique(c(code, code_parent)))) %>%
+    dplyr::left_join(tbl_hierarchy, by = "code") %>%
+    dplyr::mutate(value = ifelse(is.na(value), 0, value),
            layer_no = ifelse(is.na(layer_no), 0, layer_no),
            layer_no = factor(layer_no))
 
